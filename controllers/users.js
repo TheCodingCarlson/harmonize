c25cf036b4ca8581fe0635ce23042c3803b01b8c
Christian Carlson <chris.carlson.dev@gmail.com>
2015-12-17 15:07:07 -0800

    adding styling fixing bugs

--- controllers/users.js
+++ controllers/users.js
@@ -20,7 +20,6 @@ router.route('/')
 router.get('/:id', function(req, res) {
   User.findById(req.params.id, function(err, user) {
     if (err) return res.status(500).send(err);
-    console.log(req.params.id);
     res.send(user);
   });
 });
